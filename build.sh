73aa4d90e7c47643332a79201e32fb4dcd2e9cdb
Timothe-2004 <timothejames2004@gmail.com>
2025-06-28 11:41:17 +0100

    Deployements

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-# Script de démarrage pour Render
+# Script de build pour Render
 
 set -o errexit  # Exit on error
+
+# Installer les dépendances Python
+pip install -r requirements.txt
 
 # Collecter les fichiers statiques
 python manage.py collectstatic --noinput
 
 # Exécuter les migrations
 python manage.py migrate
-
-# Démarrer le serveur Gunicorn
-gunicorn tontiflex.wsgi:application
